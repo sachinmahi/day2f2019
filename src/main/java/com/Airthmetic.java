712f0a3fe68395216ce76ece651e52c848ce3b30
simar mahi <simarmahi@Simars-MacBook-Air.local>
2019-09-19 18:45:42 -0400

    Initial commit

---
+++ src/main/java/com/Airthmetic.java
@@ -0,0 +1,18 @@
+package com;
+
+public class Airthmetic {
+    int add(int a,int b)
+    {
+        int c;
+        c=a+b;
+        return c;
+    }
+    int sub(int d, int e)
+    {
+        int f;
+        f=d-e;
+        return f;
+
+    }
+    int multiply(int g,int h)
+}
